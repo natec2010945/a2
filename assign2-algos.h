0dec4a11c864345d60474bacf8170eae812f0b5a
ncamorli2000 <z2010945@students.niu.edu>
2023-09-15 21:01:27 -0700

    as far as I could go

--- assign2-algos.h
+++ assign2-algos.h
@@ -59,8 +59,7 @@ auto range_minval(ITERATOR begin, ITERATOR end) {
     }
   return  min;
   // XXX This has not yet been implemented. Replace this comment with code.
-  //#return -1;
-  //return ITERATOR.begin();
+  
 }
 
 template <typename ITERATOR>
@@ -74,8 +73,13 @@ int range_count(ITERATOR begin, ITERATOR end) {
 }
 
 template <typename ITERATOR>
+//print_range(std::cout, s_info.begin(), s_info.end(), "{ ", ", ", " }\n", 0);
 void print_range(std::ostream & ost, ITERATOR begin, ITERATOR end, const std::string &pre, const std::string &sep, const std::string &post, int width) {
-  // XXX This has not yet been implemented. Replace this comment with code.
+  ost << pre;
+  for (auto ITER = begin; ITER != end; ++ITER){
+    ost << std::setw(width) << *ITER << sep;
+  }
+  //ost  << post;
 }
 
 template<typename ITERATOR, typename RANDOM_ACCESS>
@@ -86,7 +90,20 @@ void histogram(ITERATOR begin, ITERATOR end, RANDOM_ACCESS & nums, int N, int di
 
 template <typename ITERATOR, typename T>
 iter_token_info<ITERATOR> get_next_token_strict(ITERATOR begin, ITERATOR end, T delim) {
-  // XXX This has not yet been implemented. Replace this comment with code.
+  /*
+  iter_token_info <ITERATOR> q;
+  q.b = begin;
+  q.e = end;
+
+  for (auto z = begin; z!= end; z++){
+    q.n = q.b + 1;
+    if (*q.n == delim){
+      q.e = q.n;
+    }
+    q.b = q.n
+  }
+  return q;
+  */
 }
 
 template <typename ITERATOR, typename T>
