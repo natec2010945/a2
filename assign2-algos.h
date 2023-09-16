578fe79715b8d91c0f21e6da811dde48f5170e6a
ncamorli2000 <z2010945@students.niu.edu>
2023-09-15 17:24:48 -0700

    everything working besides strings file

--- assign2-algos.h
+++ assign2-algos.h
@@ -1,7 +1,6 @@
 #ifndef NIUCSCI340ASSIGN2_ALGOS_H
 #define NIUCSCI340ASSIGN2_ALGOS_H
 
-#warning These programs will not work properly until you have implemented the template functions in assign2-algos.h. Once you have, remove this #warning line from the file. In the beginning, there will likely be more warnings and errors after this because of the missing implementations.
 
 #include <iomanip>
 #include <iostream>
@@ -9,36 +8,69 @@
 #include "assign2-algos.decl.h"
 
 // Implement your algorithms for assignment 2 in this file.
-
+using namespace std;
 
 template <typename ITERATOR>
 int range_sum(ITERATOR begin, ITERATOR end) {
-  // XXX This has not yet been implemented. Replace this comment with code.
-  return -1;
+  int sum = 0;
+  for (auto z = begin; z != end; ++z){
+    sum += *z;
+    //lets just see what z is?
+    }
+  return sum;
+
 }
 
 template <typename ITERATOR>
 double range_avg(ITERATOR begin, ITERATOR end) {
+  int sum = 0;
+  int counter = 0;
+  double avg = 0;
+  for (auto z = begin; z != end; ++z){
+    sum += *z;
+    counter +=1;
+    //lets just see what z is?
+    }
+    avg = sum / counter;
+  return avg;
   // XXX This has not yet been implemented. Replace this comment with code.
-  return -1;
+  //return ITERATOR.begin();
 }
 
 template <typename ITERATOR>
-int range_maxval(ITERATOR begin, ITERATOR end) {
-  // XXX This has not yet been implemented. Replace this comment with code.
-  return -1;
+auto range_maxval(ITERATOR begin, ITERATOR end) {
+  int max = 0;
+  for (auto z = begin; z != end; ++z){
+    if (*z > max){
+      max = *z;
+    }
+    //lets just see what z is?
+    }
+  return  max;
 }
 
 template <typename ITERATOR>
-int range_minval(ITERATOR begin, ITERATOR end) {
+auto range_minval(ITERATOR begin, ITERATOR end) {
+  int min = 99999999;
+  for (auto z = begin; z != end; ++z){
+    if (*z < min){
+      min = *z;
+    }
+    }
+  return  min;
   // XXX This has not yet been implemented. Replace this comment with code.
-  return -1;
+  //#return -1;
+  //return ITERATOR.begin();
 }
 
 template <typename ITERATOR>
 int range_count(ITERATOR begin, ITERATOR end) {
-  return -1;
-  // XXX This has not yet been implemented. Replace this comment with code.
+  int counter = 0;
+  for (auto z = begin; z != end; ++z){
+    counter +=1;
+    }
+  return counter;
+
 }
 
 template <typename ITERATOR>
